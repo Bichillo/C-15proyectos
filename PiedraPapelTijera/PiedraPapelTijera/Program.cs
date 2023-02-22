18e6e9428959cbf0b2c840d36614d088b07ea699
Alejandro <molina.ramos@hotmail.com>
2023-02-22 19:47:21 +0000

    first commit

---
+++ PiedraPapelTijera/PiedraPapelTijera/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Hello, World!");
+
+while(true)
+{
+    Console.WriteLine("Estas listo'");
+    Console.WriteLine("Empezamos!!");
+
+    var seleccionusuario = SeleccionDeUsuario();
+    var tuEleccion = char.Parse(seleccionusuario);
+
+    Console.WriteLine($"Elegiste {tuEleccion}");
+
+    var opcionDeOponente = OpcionOponente();
+
+    Console.WriteLine($"Tu oponente eligió {opcionDeOponente}");
+}
+
+string SeleccionDeUsuario()
+{
+    Console.WriteLine("Elige Piedra(R), Papel(P) o Tijera(T): ");
+    var seleccionUsuario = Console.ReadLine();
+
+    if (seleccionUsuario?.ToLower() != "r" && seleccionUsuario?.ToLower() != "p" && seleccionUsuario?.ToLower() != "t")
+    {
+        Console.WriteLine("Por favor seleciona solo una letra; R, P o T");
+        seleccionUsuario = SeleccionDeUsuario();
+    }
+
+    return seleccionUsuario;
+}
+
+char OpcionOponente()
+{
+    char[] opciones = new char[] { 'R', 'P', 'T' };
+
+    Random random = new Random();
+    int randomIndex = random.Next(0, opciones.Length);
+
+    return opciones[randomIndex];
+}
