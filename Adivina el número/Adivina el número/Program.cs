58cd12d5c71984f5a3b5a6ab37571cb1d96764a3
Alejandro <molina.ramos@hotmail.com>
2023-02-23 20:13:53 +0000

    comienzo nuevo

---
+++ Adivina el número/Adivina el número/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Adivina el nùmero");
+
+StartGame();
+
+void StartGame()
+{
+    Console.WriteLine("Hola, Bienvenido al juego de Adivina el número...");
+    Console.WriteLine("Como te llamas?");
+
+    var namePlayer = Console.ReadLine();
+    Console.WriteLine($"Hola {namePlayer} estás listo? (Escribe SI/NO)");
+
+    var wantToPlay = Console.ReadLine();
+
+    switch (wantToPlay?.ToLower())
+    {
+        case "yes":
+            Play();
+            break;
+        case "no":
+            DontPlay();
+            break;
+        default:
+            Console.WriteLine("Por favor, deja de hacer el troll y escribe sólo SI o NO");
+            break;
+    }
+}
+
+void Play()
+{
+
+}
+
+void DontPlay()
+{
+
+}
+
